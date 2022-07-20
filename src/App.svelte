c85cabedcfa04cc3e9367f9d93c2bcc06a4fa9ba
ttnppedr <priest0204@gmail.com>
2022-07-20 09:09:38 +0800

    fix prettier format

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,24 @@
 <script>
-    import "./css/style.css"
+    import './css/style.css';
 </script>
 
 <div class="todo-app-container">
     <div class="todo-app">
         <h2>Todo App</h2>
         <form action="#">
-            <input type="text" class="todo-input" placeholder="What do you need to do?">
+            <input type="text" class="todo-input" placeholder="What do you need to do?" />
         </form>
 
         <ul class="todo-list">
             <li class="todo-item-container">
                 <div class="todo-item">
-                    <input type="checkbox">
+                    <input type="checkbox" />
                     <span class="todo-item-label">Finish Svelte Series</span>
                 </div>
 
                 <button class="x-button">
-                    <svg
-                        class="x-button-icon"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M6 18L18 6M6 6l12 12"
-                        />
+                    <svg class="x-button-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </li>
