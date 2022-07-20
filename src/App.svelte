25a70c460d741a6f20bf9f909292eb410a86c2b9
ttnppedr <priest0204@gmail.com>
2022-07-20 14:34:03 +0800

    implement todo function

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,96 @@
 <script>
     import './css/style.css';
+
+    let todoTitle = '';
+    let todoId = 4;
+    let todos = [
+        {
+            id: 1,
+            title: 'Finish Svelte screencast',
+            isComplete: false,
+            isEditing: false,
+        },
+        {
+            id: 2,
+            title: 'Go shopping',
+            isComplete: true,
+            isEditing: false,
+        },
+        {
+            id: 3,
+            title: 'Take over world',
+            isComplete: false,
+            isEditing: false,
+        },
+    ];
+
+    $: remainingTodos = todos.filter((todo) => !todo.isComplete).length;
+
+    function addTodo() {
+        todos = [
+            ...todos,
+            {
+                id: todoId++,
+                title: todoTitle,
+                isComplete: false,
+                isEditing: false,
+            },
+        ];
+
+        todoTitle = '';
+    }
+
+    function deleteTodo(id) {
+        todos = todos.filter((todo) => todo.id !== id);
+    }
+
+    function checkAllTodos() {
+        todos = todos.map((todo) => {
+            todo.isComplete = true;
+            return todo;
+        });
+    }
+
+    function clearCompleted() {
+        todos = todos.filter((todo) => !todo.isComplete);
+    }
 </script>
 
 <div class="todo-app-container">
     <div class="todo-app">
         <h2>Todo App</h2>
-        <form action="#">
-            <input type="text" class="todo-input" placeholder="What do you need to do?" />
+        <form action="#" on:submit|preventDefault={addTodo}>
+            <input type="text" class="todo-input" placeholder="What do you need to do?" bind:value={todoTitle} />
         </form>
 
         <ul class="todo-list">
-            <li class="todo-item-container">
-                <div class="todo-item">
-                    <input type="checkbox" />
-                    <span class="todo-item-label">Finish Svelte Series</span>
-                </div>
+            {#each todos as todo}
+                <li class="todo-item-container">
+                    <div class="todo-item">
+                        <input type="checkbox" bind:checked={todo.isComplete} />
+                        <span class="todo-item-label" class:line-through={todo.isComplete}>{todo.title}</span>
+                    </div>
 
-                <button class="x-button">
-                    <svg class="x-button-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </li>
+                    <button class="x-button" on:click={deleteTodo(todo.id)}>
+                        <svg class="x-button-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M6 18L18 6M6 6l12 12"
+                            />
+                        </svg>
+                    </button>
+                </li>
+            {/each}
         </ul>
 
         <div class="check-all-container">
             <div>
-                <div class="button">Check All</div>
+                <button class="button" on:click={checkAllTodos}>Check All</button>
             </div>
 
-            <span>3 items remaining</span>
+            <span>{remainingTodos} items remaining</span>
         </div>
 
         <div class="other-buttons-container">
@@ -39,7 +100,7 @@
                 <button class="button filter-button">Completed</button>
             </div>
             <div>
-                <button class="button">Clear completed</button>
+                <button class="button" on:click={clearCompleted}>Clear completed</button>
             </div>
         </div>
     </div>
